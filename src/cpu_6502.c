993bd90044ad02feacfc2f1d29bd8a0b12f79273
lvntky <klevent1903@gmail.com>
2021-06-21 02:50:10 +0300

    stack size added

---
+++ src/cpu_6502.c
@@ -0,0 +1,10 @@
+#include "../include/cpu_6502.h"
+#include <stdio.h>
+/** Global variables **/
+static const STACK_START_ADDRESS = 0x100;
+
+int main(int argc, char *argv[])
+{
+  printf("test\n");
+  return 0;
+}
